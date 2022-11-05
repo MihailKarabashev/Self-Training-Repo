9a7fb3d75ebb264e65ce621eb6a0d283bfe876ed
mihail-karabashev <mihailkarabashev@gmail.com>
2022-11-05 15:50:17 +0200

    Add PiplineBehavior

--- CqrsMediatR/Program.cs
+++ CqrsMediatR/Program.cs
@@ -1,4 +1,5 @@
 using CqrsMediatR;
+using CqrsMediatR.Behaviors;
 using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddMediatR(typeof(Program));
-
+builder.Services.AddSingleton(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
 builder.Services.AddSingleton<FakeDataStore>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
