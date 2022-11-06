307191a4d38c5ee49e172583ca596e5f076bf762
mihail-karabashev <mihailkarabashev@gmail.com>
2022-11-06 12:18:40 +0200

    Skip and SkipWhile

--- AdvancedLINQ/Program.cs
+++ AdvancedLINQ/Program.cs
@@ -11,5 +11,26 @@ using AdvancedLINQ.Joining.InnerJoin;
 
 //ConvertTest.GetAdministratorsAsString();
 
- InnerJoinTest.Join(EmployeesCollection.Employees,DirectorsCollection.Directors);
+//InnerJoinTest.Join(EmployeesCollection.Employees,DirectorsCollection.Directors);
 
+//Partitioning Operations
+
+//Skip
+var list = new List<int>() { 2, 7, 2, 4, 5, 8, 9, 6, 1, 8, 9, 7 };
+
+Console.Write("Skip=>>>" + " ");
+foreach (var item in list.Skip(7))
+{
+    Console.Write(item + " ");
+}
+
+Console.WriteLine();
+
+
+Console.Write("SkipWhile=>>>" + " ");
+
+foreach (var item in list.SkipWhile(x=> x<9))
+{
+    Console.Write(item + " ");
+
+}
