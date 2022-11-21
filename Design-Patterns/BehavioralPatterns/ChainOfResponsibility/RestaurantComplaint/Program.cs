9bd490da4d1d5ba4e7ab717ec095f9dc15006214
mihail-karabashev <mihailkarabashev@gmail.com>
2022-11-21 14:38:52 +0200

    Update employee variable to waiter variable

--- Design-Patterns/BehavioralPatterns/ChainOfResponsibility/RestaurantComplaint/Program.cs
+++ Design-Patterns/BehavioralPatterns/ChainOfResponsibility/RestaurantComplaint/Program.cs
@@ -2,16 +2,16 @@
 using RestaurantComplaint.Models;
 using RestaurantComplaint.Enums;
 
-var employee = RestaurantChain.GetEmployee();
+var waiter = RestaurantChain.GetEmployee();
 
 var complaint1 = new Complaint() { CustomerName = "Ivan", Priority = Priority.Low };
-employee.ProcessComplaint(complaint1);
+waiter.ProcessComplaint(complaint1);
 
 var complaint2 = new Complaint() { CustomerName = "Dragan", Priority = Priority.Medium };
-employee.ProcessComplaint(complaint2);
+waiter.ProcessComplaint(complaint2);
 
 var complaint3 = new Complaint() { CustomerName = "Petkan", Priority = Priority.High };
-employee.ProcessComplaint(complaint3);
+waiter.ProcessComplaint(complaint3);
 
 var complaint4 = new Complaint() { CustomerName = "NoName", Priority = Priority.Crutial };
-employee.ProcessComplaint(complaint4);
+waiter.ProcessComplaint(complaint4);
